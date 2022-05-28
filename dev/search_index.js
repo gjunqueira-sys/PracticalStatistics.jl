18a65ce2d96087bfd6a29e96a61d2545c170e7a3
Documenter.jl <documenter@juliadocs.github.io>
2022-05-28 20:45:23 +0000

    build based on 4c534a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PracticalStatistics","category":"page"},{"location":"#PracticalStatistics","page":"Home","title":"PracticalStatistics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PracticalStatistics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PracticalStatistics]","category":"page"}]
+}
